3bc174d2252e09d868ec04f5f76ebec2d76e8361
Kwan Rimorin John Eric <40650158+rimorin@users.noreply.github.com>
2025-03-23 23:51:24 +0800

    fix: allow for parentheses when updating territory code (#37)

--- src/utils/helpers/checkterritorycd.ts
+++ src/utils/helpers/checkterritorycd.ts
@@ -1,6 +1,6 @@
 const isValidTerritoryCode = (territoryCd: string) => {
-  // check if code is not alphanumeric, space, or dash
-  if (!/^[a-zA-Z0-9- ]*$/.test(territoryCd)) {
+  // check if code is not alphanumeric, space, dash, or parentheses
+  if (!/^[a-zA-Z0-9- ()]*$/.test(territoryCd)) {
     return false;
   }
   return true;
