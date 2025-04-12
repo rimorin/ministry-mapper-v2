247bf9956bf856291899696ed47695e4e03c5343
Kwan Rimorin John Eric <40650158+rimorin@users.noreply.github.com>
2025-04-12 22:40:00 +0800

    fix: ignore pb cancellation error (#43)

--- src/utils/helpers/errorhandler.ts
+++ src/utils/helpers/errorhandler.ts
@@ -2,7 +2,13 @@ import * as Sentry from "@sentry/react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const errorHandler = (error: any, showAlert = true) => {
+  // If it's an aborted request, we don't need to report it
+  if (error.isAbort) {
+    console.warn("Request was aborted:", error);
+    return;
+  }
   Sentry.captureException(error);
+  // Show alert to user if requested
   if (showAlert) {
     const errorStatus = error.status || "Error";
     if (error.response && error.response.message) {
