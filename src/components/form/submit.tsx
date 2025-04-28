d8afdae138bdc2fd5a6f4c12ee3d44da21cc2262
John Eric <rimorin@gmail.com>
2025-04-28 21:02:14 +0800

    fix: i18n translation issue

--- src/components/form/submit.tsx
+++ src/components/form/submit.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 const ModalSubmitButton = ({
   isSaving = false,
-  btnLabel = "common.save",
+  btnLabel,
   disabled = false
 }: SubmitBtnProps) => {
   const { t } = useTranslation();
