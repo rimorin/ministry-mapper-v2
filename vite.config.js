e14b1ec36b466f8f088ebd77994d776b82edebc4
John Eric <rimorin@gmail.com>
2025-09-24 00:18:56 +0800

    feat: pwa implementation

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import TurboConsole from "unplugin-turbo-console/vite";
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig(() => {
   return {
     build: {
@@ -22,7 +23,39 @@ export default defineConfig(() => {
     server: {
       port: 3000
     },
-    plugins: [react(), visualizer(), TurboConsole()],
+    plugins: [react(), visualizer(), TurboConsole(), VitePWA({
+      registerType: "autoUpdate",
+      manifest: false,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/assets\.ministry-mapper\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "external-assets",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/maps\.googleapis\.com\/.*/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "google-maps",
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 7 // 7 days
+              }
+            }
+          }
+        ],
+        navigateFallback: "/index.html",
+        navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/]
+      }
+    })],
     css: {
       preprocessorOptions: {
         scss: {
